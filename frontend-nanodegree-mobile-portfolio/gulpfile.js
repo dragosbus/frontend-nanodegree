93ce5379b0ec939f4e6107ea2e9311cb9cf97fa4
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-05-23 08:49:06 +0000

    initial commit

---
+++ frontend-nanodegree-mobile-portfolio/gulpfile.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var gulp = require("gulp");
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var imagemin = require('gulp-imagemin');
+
+
+gulp.task('default', function () {
+    gulp.watch("*.html").on('change', browserSync.reload);
+
+    browserSync.init({
+        server: './'
+    });
+});
+
+gulp.task('imagemin', () =>
+    gulp.src('img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task('scripts', function () {
+    gulp.src('js/**/*.js')
+        .pipe(babel())
+        .pipe(concat(app.js))
+        .pipe(gulp.dest('dist/js'));
+});
