3a5204622a2994a39fd804d7f0f95e0171227c9e
dragosbus <dragosbusuioc94@gmail.com>
2018-05-26 13:35:41 +0200

    bounce the marker clicked

--- neighborhood-map/js/app.js
+++ neighborhood-map/js/app.js
@@ -33,6 +33,7 @@ const UI = (function () {
             else reject("Invalid Location");
         }).then(res => {
             getData(res).then(res => {
+                locationsList.innerHTML = '';
                 map = new google.maps.Map(document.getElementById('map'), {
                     center: {
                         lat: res.response.geocode.feature.geometry.center.lat,
@@ -43,7 +44,7 @@ const UI = (function () {
                 res.response.venues.forEach(v => {
                     locationsList.appendChild(makeLi(v.name));
                     console.log(v);
-                    addMarker(map, v.location.lat, v.location.lng);
+                    addMarker(map, v);
                 });
             });
         });
@@ -55,13 +56,50 @@ const UI = (function () {
         return li;
     }
 
-    function addMarker(map, lat, long) {
-        let pos = { lat:lat, lng:long };
+    function addMarker(map, data) {
+        var largeInfowindow = new google.maps.InfoWindow();
+        let pos = { lat: data.location.lat, lng: data.location.lng };
         var marker = new google.maps.Marker({
             position: pos,
             map: map,
-            title: 'First Marker!'
+            title: data.name,
+            category: data.categories[0].pluralName,
+            address: data.location.formattedAddress[0],
+            animation: google.maps.Animation.DROP,
         });
+        marker.addListener('click', function () {
+            populateInfoWindow(this, largeInfowindow);
+            toggleBounce(this);
+        });
+    }
+
+    function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        let content = `<div>
+        <h4>${marker.title}</h4>
+        <div>
+            <p>Category:${marker.category}</p>
+            <p>Address:${marker.address}</p>
+        </div>
+        </div>`;
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent(content);
+            infowindow.open(map, marker);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function () {
+                infowindow.setMarker = null;
+                toggleBounce(marker);
+            });
+        }
+    }
+
+    function toggleBounce(marker) {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
     }
    
     return { init }; 
