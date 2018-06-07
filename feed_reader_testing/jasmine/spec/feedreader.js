669fc9c6c233655768a2000960401f1f5953cba2
dragosbus <dragosbusuioc94@gmail.com>
2018-06-07 10:40:18 +0200

    finished all tests

--- feed_reader_testing/jasmine/spec/feedreader.js
+++ feed_reader_testing/jasmine/spec/feedreader.js
@@ -74,18 +74,40 @@ $(function() {
         });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+        describe("Initial entries", ()=>{
+            beforeEach(function(done) {
+                loadFeed(0, done);
+            });
+            /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+            it('are present', function() {
+                expect($('.feed .entry').length).toBeGreaterThan(0);
+            });
+        });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        var oldFeed;
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                // store old feed
+                oldFeed = $('.feed').html();
+                // fetch newer feed
+                loadFeed(1, done);
+            });
+        });
+
+        it('is different from old', function() {
+            expect($('.feed').html()).not.toBe(oldFeed);
+        });
+    });
 }());
